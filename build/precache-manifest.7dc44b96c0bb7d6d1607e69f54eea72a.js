db33d9d6cde4ab1baf8907c45d87343f10791c49
Jiemba <Jiemba@aihealth.tech>
2020-09-28 01:32:57 +1000

    Build include

---
+++ build/precache-manifest.7dc44b96c0bb7d6d1607e69f54eea72a.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175c6b31b501817d14cffae4b667eee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da020d7ffae3212a6a6",
+    "url": "/static/css/2.959f4f2f.chunk.css"
+  },
+  {
+    "revision": "a946c682d20732e6bab2",
+    "url": "/static/css/main.29691f73.chunk.css"
+  },
+  {
+    "revision": "4da020d7ffae3212a6a6",
+    "url": "/static/js/2.bbd54cd4.chunk.js"
+  },
+  {
+    "revision": "96d38864f001523e60e81c4d395f01a8",
+    "url": "/static/js/2.bbd54cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a946c682d20732e6bab2",
+    "url": "/static/js/main.4cfba75c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "0a50b4f689fec78b7f6a0a2cb3c26899",
+    "url": "/static/media/android.0a50b4f6.svg"
+  },
+  {
+    "revision": "ce13ad56b9e7d01e2b67adec07ac49a2",
+    "url": "/static/media/angular.ce13ad56.svg"
+  },
+  {
+    "revision": "1bf8f5e161574ee8f9182ae172472de9",
+    "url": "/static/media/api.1bf8f5e1.svg"
+  },
+  {
+    "revision": "36d651eb741c2c8a6835147370cbe20d",
+    "url": "/static/media/aws.36d651eb.svg"
+  },
+  {
+    "revision": "bb7b9270c2d17933530407b6ac4656cd",
+    "url": "/static/media/azure.bb7b9270.svg"
+  },
+  {
+    "revision": "9a1726755180f74d7a1dd9a059af0c58",
+    "url": "/static/media/botsito.9a172675.png"
+  },
+  {
+    "revision": "18713802ffbb3902aded25597dc4fcb7",
+    "url": "/static/media/cplus.18713802.svg"
+  },
+  {
+    "revision": "a3b50bf0e191e62fe9514bccb1bf6a9c",
+    "url": "/static/media/csharp.a3b50bf0.svg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a1d5024611c72f9600cdef232a529711",
+    "url": "/static/media/github.a1d50246.svg"
+  },
+  {
+    "revision": "538bed1f49e5567d5553055967f50472",
+    "url": "/static/media/html.538bed1f.svg"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "48f5542cedb6884b0a0867245d022e46",
+    "url": "/static/media/java.48f5542c.svg"
+  },
+  {
+    "revision": "c0f2d9b5390012da07bcaa79cd3052fa",
+    "url": "/static/media/javascript.c0f2d9b5.svg"
+  },
+  {
+    "revision": "b4edc9e7e72c5e5ea7174ace924346a1",
+    "url": "/static/media/jquery.b4edc9e7.svg"
+  },
+  {
+    "revision": "e169e075d4b8bf01bcb77b52f7d5a7c6",
+    "url": "/static/media/mysql.e169e075.svg"
+  },
+  {
+    "revision": "946a088b4e57b2358ec4c28f562d4150",
+    "url": "/static/media/net.946a088b.svg"
+  },
+  {
+    "revision": "ed53422436e524ca8323c6c2c265da3e",
+    "url": "/static/media/nodejs.ed534224.svg"
+  },
+  {
+    "revision": "47a436ca020e380abc48e9a89ee330d8",
+    "url": "/static/media/oracle.47a436ca.svg"
+  },
+  {
+    "revision": "278d6eb276459607729b37f9b84cba8d",
+    "url": "/static/media/php.278d6eb2.svg"
+  },
+  {
+    "revision": "ac7071389eec8096d6f80cc8a34ef683",
+    "url": "/static/media/python.ac707138.svg"
+  },
+  {
+    "revision": "d0e62cd0e73e90d18556e566d73fc6bc",
+    "url": "/static/media/react.d0e62cd0.svg"
+  },
+  {
+    "revision": "67511de0d765aed902cdebafe96aa139",
+    "url": "/static/media/sql.67511de0.svg"
+  },
+  {
+    "revision": "fed15b16f2fd3481bdd5587734822b38",
+    "url": "/static/media/wordpress.fed15b16.svg"
+  }
+]);
